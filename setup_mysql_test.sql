01279a276eb56646ea8b66e2181ea61b8609dd80
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-22 21:20:50 +0000

    task4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- Create states table in hbtn_0e_4_usa with some data
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS performance_schema;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
