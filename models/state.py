f3b217875c576d7e301a4e3e5c6ccbc30004f79d
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-24 19:10:17 +0000

    revised classes

--- models/state.py
+++ models/state.py
@@ -2,12 +2,16 @@
 '''
     Implementation of the State class
 '''
-
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
+from models.base_model import Base
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     '''
         Implementation for the State.
     '''
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128)), nullable=False)
+    cities - relationship('City', back_populates='City')
