9af857e6d588327885bdff19ca502b1d2caec883
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-25 19:54:40 +0000

    task8

--- models/place.py
+++ models/place.py
@@ -12,13 +12,14 @@ class Place(BaseModel):
     '''
         Define the class Place that inherits from BaseModel.
     '''
+    __tablename__ = "places"
     city_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(Integer, default=0)
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
-    latitude = Column(Float, default=0.0)
-    longitude = Column(Float, default=0.0)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True, default=0.0)
+    longitude = Column(Float, nullable=True, default=0.0)
