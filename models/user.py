ee5742e0e2d8eb33ce8c3012fad1ef08dcfb6505
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-25 20:39:12 +0000

    delete typo

--- models/user.py
+++ models/user.py
@@ -3,18 +3,23 @@
     Implementation of the User class which inherits from BaseModel
 '''
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+
 
 class User(BaseModel, Base):
     '''
         Definition of the User class
     '''
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship('Place', back_populates='user', cascade='all, delete-orphan')
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", cascade="all, delete-orphan")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
