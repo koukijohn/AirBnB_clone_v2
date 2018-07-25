fe1e2049085360d1d6bff725a53a71ce67ce328f
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2018-07-25 20:28:41 +0000

    revise relationship

--- models/user.py
+++ models/user.py
@@ -3,10 +3,8 @@
     Implementation of the User class which inherits from BaseModel
 '''
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
 
 class User(BaseModel, Base):
     '''
@@ -15,11 +13,8 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-<<<<<<< HEAD
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    places = relationship('User', back_populates='User', cascade='all, delete-orphan')
-=======
+    places = relationship('Place', back_populates='user', cascade='all, delete-orphan')
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
->>>>>>> 07ef153baa05b80be4df2cc57605040bb29fa5da
