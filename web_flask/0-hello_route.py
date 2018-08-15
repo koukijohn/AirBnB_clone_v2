16fc751fdaba601dc6b1dd2899bde9e4aa7bf031
koukijohn <koukijohn@github.com>
2018-08-15 17:37:29 +0000

    added if __name__ == __main__

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+    This is a script that starts a Flask web application.
+"""
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +9,6 @@ app = Flask(__name__)
 def hello_world():
     return "Hello HBNB!"
 
-app.run()
+
+if __name__ == "__main__":
+    app.run()
