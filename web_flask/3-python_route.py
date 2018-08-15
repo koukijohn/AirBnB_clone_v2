ebcc06f7389a1c8e97791a4053fc2937b4917539
koukijohn <koukijohn@github.com>
2018-08-15 18:10:00 +0000

    added 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
+"""
+    This is a script that starts a Flask web application and will display:
+    / will display “Hello HBNB!”, /hbnb will display “HBNB”, /c/<text>
+    will display “C ” followed by value of the text variable w/out a space,
+    /python/<text> will display “Python ”, followed by value of text variable.
+"""
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c(text):
@@ -21,4 +30,5 @@ def display_python(text="is cool"):
     return "Python {}".format(text).replace("_", " ")
 
 
-app.run()
+if __name__ == "__main__":
+    app.run()
