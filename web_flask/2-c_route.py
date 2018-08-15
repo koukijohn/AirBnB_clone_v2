1173c5dc3eb40cb6b8356b6706e916fa47b015bf
koukijohn <koukijohn@github.com>
2018-08-15 17:49:33 +0000

    added if __name__ == __main_-, doctest comment, and fixed pep8 errors

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
+"""
+    This is a script that starts a Flask web application and will display:
+    / will display “Hello HBNB!”, /hbnb will display “HBNB”, and /c/<text>
+    will display “C ” followed by value of the text variable w/out a space.
+"""
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c(text):
     return "C {}".format(text).replace("_", " ")
 
-app.run()
+if __name__ == "__main__":
+    app.run()
