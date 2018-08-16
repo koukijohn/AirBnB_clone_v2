cff858e85cec8c64f8d05d86d01a47fc019fa628
koukijohn <koukijohn@github.com>
2018-08-16 00:38:43 +0000

    fixed pep8

--- models/state.py
+++ models/state.py
@@ -18,7 +18,6 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
 
-
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='State',
